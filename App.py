2c41e05d853a45e8b16ed93b194a0165fcbf455c
Leva-byte <levaransu.0920@gmail.com>
2026-03-14 13:44:04 +0800

    Remove Base64 Logs reflecting on Railway Logs

--- App.py
+++ App.py
@@ -90,8 +90,8 @@ socketio = SocketIO(
     app,
     cors_allowed_origins="*",
     async_mode='threading',
-    logger=True,
-    engineio_logger=True,
+    logger=False,
+    engineio_logger=False,
     ping_timeout=60,
     ping_interval=25
 )
